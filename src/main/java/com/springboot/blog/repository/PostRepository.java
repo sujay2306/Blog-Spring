fb1afc229275f84d3c0c230123a472963436b2a5
sujay2306 <sujayks007@gmail.com>
2023-04-09 19:08:49 +0530

    added exception handling

---
+++ src/main/java/com/springboot/blog/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.blog.repository;
+
+import com.springboot.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//jpa repo is generic so we gave a type post as entity type we have given key data type id as long
+// in post so we have given long here
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
