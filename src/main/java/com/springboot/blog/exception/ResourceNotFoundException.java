77750914a1714c3268225e9c61b01dbca8236c53
sujay2306 <sujayks007@gmail.com>
2023-04-11 21:49:58 +0530

    added apis

--- src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.springboot.blog.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)            //returns status code to client
 public class ResourceNotFoundException extends RuntimeException{
     private String resourceName;
     private String fieldName;
@@ -10,5 +14,17 @@ public class ResourceNotFoundException extends RuntimeException{
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
     }
 }
